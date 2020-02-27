6301a15883533e66c602aaf4348f802be609e38f
GMorse19 <malorse@comcast.net>
2020-02-27 09:56:50 -0500

    Adjusted ChoiceForm

--- src/components/Choices/ChoiceForm.js
+++ src/components/Choices/ChoiceForm.js
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 const ChoiceForm = ({ props, subjectId, choice, handleSubmit, handleChange, cancelPath }) => (
-  <div className="subject-board row">
+  <div className="subject-board row body">
     <div className="col-sm-10 col-md-8 mx-auto mt-5">
       <h1>Create A Choice</h1>
       <Form onSubmit={handleSubmit}>
