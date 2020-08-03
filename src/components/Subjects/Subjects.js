ddadaba5054d817025e7a404087299e15d61793c
GMorse19 <malorse@comcast.net>
2020-08-02 20:00:55 -0400

    Refactored subject/poll messaging

--- src/components/Subjects/Subjects.js
+++ src/components/Subjects/Subjects.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-import Row from 'react-bootstrap/Row'
-import Container from 'react-bootstrap/Container'
+import SubjectsReturn from '../../helpers/subjectsReturn'
 
 import { getSubjects } from '../../api/subjects'
 import { jsx } from '../../helpers/jsx'
@@ -18,19 +17,10 @@ const Subjects = props => {
   const subjectsJsx = jsx(subjects)
 
   return (
-    <div className="page-content">
-      <div style={{ textAlign: 'center' }}>
-        <h1>Choose A Poll</h1>
-        <p>Add a choice to a Poll or cast a vote!</p>
-      </div>
-      <div className='directory-menu'>
-        <Container>
-          <Row className="box">
-            {subjectsJsx}
-          </Row>
-        </Container>
-      </div>
-    </div>
+    <SubjectsReturn
+      user={props.user}
+      subjectsJsx={subjectsJsx}
+    />
   )
 }
 
